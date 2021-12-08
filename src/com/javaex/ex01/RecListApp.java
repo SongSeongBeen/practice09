728e8d4442663300985aac7272439b6704b6b4f1
송성빈 <aktm1004@naver.com>
2021-12-09 00:24:51 +0900

    과제9

--- src/com/javaex/ex01/RecListApp.java
+++ src/com/javaex/ex01/RecListApp.java
@@ -23,29 +23,31 @@ public class RecListApp {
 		rList.add(r03);
 		rList.add(r04);
 		//리스트 모두를 draw하는 메소드 실행
-		for(int i=0; i<rList.size(); i++) {
-			rList.get(i).draw();
-		}
+//		for(int i=0; i<rList.size(); i++) {
+//			rList.get(i).draw();
+//		}
+		listAllDraw(rList);
 		
 		
 
 		System.out.println("#3번째 사각형(3, 3) 삭제==========================================");
 		//사각형(3,3) 삭제 코드 작성
 		rList.remove(r03);
-		//리스트 모두를 draw하는 메소드 실행
-		for(int i=0; i<rList.size(); i++) {
-			rList.get(i).draw();
-		}
+//		//리스트 모두를 draw하는 메소드 실행
+//		for(int i=0; i<rList.size(); i++) {
+//			rList.get(i).draw();
+//		}
+		listAllDraw(rList);
 
 		
 		System.out.println("#4번째 사각형(4, 5)-->(9, 5)로 width 값 변경======================");
 		//사각형(2,2) 수정 코드 작성
 		r04.setWidth(9);      //r04에 가로값만 수정
 		//리스트 모두를 draw하는 메소드 실행
-		for(int i=0; i<rList.size(); i++) {
-			rList.get(i).draw();
-		}
-		
+//		for(int i=0; i<rList.size(); i++) {
+//			rList.get(i).draw();
+//		}
+		listAllDraw(rList);
 		
 		
 		System.out.println("#모든 사각형의 넓이 합 출력=======================================");
@@ -59,8 +61,6 @@ public class RecListApp {
 		
 		
 	}
-	
-	
 	
 	//리스트 모두를 draw하는 메소드
 	public static void listAllDraw(List<Rectangle> list) {
--- src/com/javaex/ex02/FriendApp.java
+++ src/com/javaex/ex02/FriendApp.java
@@ -1,13 +1,32 @@
 package com.javaex.ex02;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FriendApp {
 
     public static void main(String[] args) {
-
-
     	
+    	Scanner sc = new Scanner(System.in);
     	
+    	List<Friend> fList = new ArrayList<Friend>();
+    
+    	System.out.println("친구 3명을 등록해 주세요");
+    	for(int i=0; i<3; i++) {
+    		String put = sc.nextLine();
+    		String[] sArray = put.split(" ");
+    		
+    		Friend f = new Friend(sArray[0], sArray[1], sArray[2]);
+    		
+    		fList.add(f);
+    	}
+    	
+    	for(Friend f:fList) {
+    		f.showInfo();
+    	}
+    	sc.close();
     }
 
 }
